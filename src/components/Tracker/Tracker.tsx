ec95e1ea66499812ecc99ebed9f250ab28678536
EngiFrost <engifrost@gmail.com>
2022-11-22 21:32:19 +0300

    improved tracker logic

--- src/components/Tracker/Tracker.tsx
+++ src/components/Tracker/Tracker.tsx
@@ -1,10 +1,25 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { TimerBar } from './TimerBar';
+
 import * as S from './styles';
 import * as H from './helpers';
 
+// TODO: get this values from input
+const DEFAULT_TIME = 5 * 60;
+const REST_TIME = 5 * 60;
+
 export const Tracker: FC = () => {
-  const [time, setTime] = useState<number>(0)
-  
+  const [time, setTime] = useState<number>(DEFAULT_TIME);
+  const [mode, setMode] = useState<'work' | 'rest'>('work');
+  const [isPaused, setPaused] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!time) {
+      mode === 'work' ? setTime(REST_TIME) : setTime(DEFAULT_TIME);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [time]);
+
   return (
     <S.TrackerWrapper>
       <S.Title>Tomato Tracker!</S.Title>
@@ -15,9 +30,9 @@ export const Tracker: FC = () => {
         <S.SettingsBtn />
       </S.InfoSection>
 
-      {/* <TimerBar time={time}/> */}
+      <TimerBar time={time} />
 
-      <S.ActionBtn />
+      <S.ActionBtn onClick={() => setPaused(!isPaused)} />
     </S.TrackerWrapper>
   );
 };
