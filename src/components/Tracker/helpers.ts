b3cd22a33d3bf0f750ad43df08d8e89c9becc30b
EngiFrost <engifrost@gmail.com>
2022-11-24 17:04:11 +0300

    improved time formatter

--- src/components/Tracker/helpers.ts
+++ src/components/Tracker/helpers.ts
@@ -1,5 +1,9 @@
 export const formatTime = (time: number): string => {
   const minutes = ~~(time / 60);
   const seconds = time % 60;
-  return `${minutes}:${seconds}`;
+  return `${makeTwoDigits(minutes)}:${makeTwoDigits(seconds)}`;
+};
+
+const makeTwoDigits = (value: number): string => {
+  return value < 10 ? `0${value}` : `${value}`;
 };
