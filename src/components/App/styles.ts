a47c85c45768e92bf295b3530460af5754b6f877
EngiFrost <engifrost@gmail.com>
2022-11-25 12:55:18 +0300

    styled bars

--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
 
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
 
   background-color: tomato;
 `;
--- src/components/TimerBar/styles.ts
+++ src/components/TimerBar/styles.ts
@@ -1,5 +1,23 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const Bar = styled.div<{ isEnlarged: boolean }>``;
+export const Bar = styled.div<{ isEnlarged: boolean }>`
+  background-color: white;
+  width: 3px;
+  border-radius: 2px;
 
-export const Wrapper = styled.div<{ offset: number }>``;
+  ${(p) =>
+    p.isEnlarged
+      ? css`
+          height: 50px;
+        `
+      : css`
+          height: 20px;
+        `}
+`;
+
+export const Wrapper = styled.div<{ offset: number }>`
+  display: flex;
+  align-items: flex-end;
+  gap: 30px;
+`;
