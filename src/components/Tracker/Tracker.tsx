94f3a546442773b30aba3500b2d38a70fab4d8b5
EngiFrost <engifrost@gmail.com>
2022-11-29 16:31:26 +0300

    created skip action function

--- src/components/Tracker/Tracker.tsx
+++ src/components/Tracker/Tracker.tsx
@@ -5,11 +5,11 @@ import * as S from './styles';
 import * as H from './helpers';
 
 // TODO: get this values from input
-const DEFAULT_TIME = 25 * 60;
-const REST_TIME = 5 * 60;
+const DEFAULT_TOMATO_TIME = 25 * 60;
+const DEFAULT_REST_TIME = 5 * 60;
 
 export const Tracker: FC = () => {
-  const [time, setTime] = useState<number>(DEFAULT_TIME);
+  const [time, setTime] = useState<number>(DEFAULT_TOMATO_TIME);
   const [mode, setMode] = useState<'tomato' | 'rest'>('tomato');
   const [isPaused, setPaused] = useState<boolean>(true);
 
@@ -17,10 +17,16 @@ export const Tracker: FC = () => {
 
   useEffect(() => {
     if (!time) {
-      mode === 'tomato' ? setTime(REST_TIME) : setTime(DEFAULT_TIME);
+      mode === 'tomato' ? setTime(DEFAULT_REST_TIME) : setTime(DEFAULT_TOMATO_TIME);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [time]);
+
+  const skipAction = () => {
+    const nextMode = mode === 'tomato' ? 'rest' : 'tomato';
+    setMode(nextMode);
+    setTime(nextMode === 'tomato' ? DEFAULT_TOMATO_TIME : DEFAULT_REST_TIME);
+  };
 
   return (
     <S.TrackerWrapper>
@@ -28,7 +34,7 @@ export const Tracker: FC = () => {
 
       <S.InfoSection>
         <S.BtnWrapper>
-          <S.Button label={`Skip ${mode}`} />
+          <S.Button label={`Skip ${mode}`} onClick={skipAction} />
         </S.BtnWrapper>
 
         <S.Timer>{H.formatTime(time)}</S.Timer>
