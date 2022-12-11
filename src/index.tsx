0cde70a915ea7f47fc4f1bd42033985ad7f60cc9
EngiFrost <engifrost@gmail.com>
2022-12-11 12:19:08 +0300

    added screen wake lock

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
+import ScreenWakeLock from 'screen-wake-lock';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <ScreenWakeLock />
     <App />
   </React.StrictMode>
 );
